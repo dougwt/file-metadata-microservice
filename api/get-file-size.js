32a6d678467b2f619d88a9dc41ff8a75d4ab505d
Doug Tyler <dougwt@gmail.com>
2019-10-01 15:54:33 -0700

    WIP handle file upload

--- api/get-file-size.js
+++ api/get-file-size.js
@@ -1,9 +1,16 @@
-// const multer  = require('multer');
-// const upload = multer({ dest: './tmp/' });
-// var autoReap  = require('multer-autoreap');
+var formidable = require('formidable');
 
-function get_file_size(req, res, next) {
-  if (!req.file) {
+function get_uploaded_files() {
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, fields, files) {
+    return files;
+  });
+}
+
+module.exports = (req, res) => {
+  var files = get_uploaded_files();
+  
+  if (!files) {
     res.status(400).json({
       error: 400,
       message: 'Missing file attachment'
@@ -11,12 +18,9 @@ function get_file_size(req, res, next) {
     return;
   }
 
-  // console.log(req.file);
-  res.send({ size: req.file.size });
-}
+  res.json({ 'size': files });
 
-module.exports = (req, res) => {
-  res.end(`Hello from /api/get-file-size/ on Now 2.0!`);
+  // res.end(`Hello from /api/get-file-size/ on Now 2.0!`);
   // res.end(JSON.stringify(req));
   // app.post('/get-file-size', upload.single('file'), autoReap, get_file_size);
 };
